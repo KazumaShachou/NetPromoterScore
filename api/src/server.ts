da7f8df45d7841d2bba2957b43201a3e14944aa3
KazumaShachou <raianimes1000@gmail.com>
2021-02-22 18:43:43 -0400

    using imsomnia

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,5 +2,22 @@ import express from 'express';
 
 const app = express();
 
+/**
+ * GET = SEARCH
+ * POST = SAVE
+ * PUT = CHANGE
+ * DELETE = DELETE
+ * PATCH => SPECIFIC CHANGES
+ */
+
+ app.get("/", (request, response) => {
+
+    return response.json({message: "Hello World"})
+ });
+
+ app.post("/", (request, response) => {
+    //receive the data to save
+    return response.json({message: "data has been saved successfully" });
+ });
 
 app.listen(3000, () => console.log("Server ta rodando fião, vamo de hello world"));
